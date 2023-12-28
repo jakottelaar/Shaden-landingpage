1dadb437cf551081fadc632aaf5b606b23f808f6
jakottelaar <jarno.kottelaar@gmail.com>
2023-12-28 20:18:10 +0100

    Updated navbar container width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="bg-secondary-100 flex justify-center">
-      <nav className="container flex flex-row justify-between p-4 px-20">
+      <nav className="container flex flex-row justify-between p-4 px-80">
         <div className="text-3xl font-bold text-white">Shaden</div>
         <button className="hover:bg-primary-100 rounded-2xl bg-white p-2 px-6 font-semibold transition-all duration-300 hover:text-white hover:shadow-lg">
           Open app
