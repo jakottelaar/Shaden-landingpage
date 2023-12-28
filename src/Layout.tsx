70bd3526fe7f04b48915bad7f4184059107c2718
jakottelaar <jarno.kottelaar@gmail.com>
2023-12-28 20:15:22 +0100

    Updated navbar and created footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="bg-primary-100 h-[500px] w-full">Footer</footer>;
+};
+
+export default Footer;
