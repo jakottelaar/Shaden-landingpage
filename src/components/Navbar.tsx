29c23e035f2aaa02cfb03a5cd0dc9f2749f17778
jakottelaar <jarno.kottelaar@gmail.com>
2024-01-04 22:09:10 +0100

    Updated Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,13 @@ import ShadenLogo from "../assets/Shaden logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="bg-secondary-100 flex justify-center">
-      <nav className="container flex flex-row justify-between p-4 px-80">
+    <div className="flex justify-center bg-secondary-100">
+      <nav className="container flex flex-row justify-between p-4 px-20">
         <div className="flex cursor-pointer flex-row space-x-4">
           <img src={ShadenLogo} className="w-[30px]" />
           <div className="text-3xl font-bold text-white">Shaden</div>
         </div>
-        <button className="hover:bg-primary-100 rounded-2xl bg-white p-2 px-6 font-semibold transition-all duration-300 hover:text-white hover:shadow-lg">
+        <button className="rounded-2xl bg-white p-2 px-6 font-semibold transition-all duration-300 hover:bg-primary-100 hover:text-white hover:shadow-lg">
           Open app
         </button>
       </nav>
